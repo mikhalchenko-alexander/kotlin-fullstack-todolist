31cbc1b0629dcefc91e26b4bd67a28f1e7ccdc50
mikhalchenko-alexander <mikhalchenko.alexander@gmail.com>
2019-08-11 13:36:31 +0300

    Add frontend module.

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,7 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.3.41"
+    id("org.jetbrains.kotlin.js") version "1.3.41"
 }
 
 group = "com.anahoret."
@@ -12,13 +10,11 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-js"))
     testCompile("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+kotlin.target.browser { }
